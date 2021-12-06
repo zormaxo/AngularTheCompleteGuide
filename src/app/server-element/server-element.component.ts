108595c5f39393852948cd1c26fed0b02df69896
zormax <zormax052@gmail.com>
2021-12-06 21:37:27 +0300

    end of lesson 73

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,12 +1,13 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css'],
+  encapsulation: ViewEncapsulation.Emulated, // None, Native
 })
 export class ServerElementComponent implements OnInit {
-  @Input("srvElement") element: { type: string; name: string; content: string };
+  @Input('srvElement') element: { type: string; name: string; content: string };
 
   constructor() {}
 
