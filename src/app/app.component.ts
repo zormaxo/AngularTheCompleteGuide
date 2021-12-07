83653efeb8677ed73735a930a61cae76779afe69
zormax <zormax052@gmail.com>
2021-12-07 18:02:35 +0300

    end of lesson 79

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,69 @@
-import { Component } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent
+  implements
+    OnInit,
+    OnChanges,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewInit,
+    AfterViewChecked,
+    OnDestroy
+{
   serverElements = [{ type: 'server', name: 'Testserver', content: 'Just a test' }];
+
+  constructor() {
+    console.log('app-component constructor called');
+  }
+
+  ngOnChanges(changes) {
+    console.log('app-component ngOnChanges');
+    console.log(changes);
+  }
+
+  ngOnInit(): void {
+    console.log('app-component ngOnInit called');
+  }
+
+  ngDoCheck() {
+    console.log('app-component ngDoCheck called');
+  }
+
+  ngAfterContentInit() {
+    console.log('app-component ngAfterContentInit called');
+  }
+
+  ngAfterContentChecked() {
+    console.log('app-component ngAfterContentChecked called');
+  }
+
+  ngAfterViewInit() {
+    console.log('app-component ngAfterViewInit called');
+  }
+
+  ngAfterViewChecked() {
+    console.log('app-component ngAfterViewChecked called');
+  }
+
+  ngOnDestroy(): void {
+    console.log('app-component ngOnDestroy called');
+  }
 
   onServerAdded(serverData: { serverName: string; serverContent: string }) {
     this.serverElements.push({
@@ -22,5 +79,13 @@ export class AppComponent {
       name: blueprintData.serverName,
       content: blueprintData.serverContent,
     });
+  }
+
+  onChangeFirst() {
+    this.serverElements[0].name = 'Changed';
+  }
+
+  onDestroyFirst() {
+    this.serverElements.splice(0,1);
   }
 }
--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,4 +1,16 @@
-import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  ViewEncapsulation,
+} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -6,10 +18,54 @@ import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./server-element.component.css'],
   encapsulation: ViewEncapsulation.Emulated, // None, Native
 })
-export class ServerElementComponent implements OnInit {
-  @Input('srvElement') element: { type: string; name: string; content: string };
+export class ServerElementComponent
+  implements
+    OnInit,
+    OnChanges,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewInit,
+    AfterViewChecked,
+    OnDestroy
+{
+  // @Input('srvElement') element: { type: string; name: string; content: string };
+  @Input() name: string;
 
-  constructor() {}
+  constructor() {
+    console.log('server-element constructor called');
+  }
 
-  ngOnInit(): void {}
+  ngOnChanges(changes) {
+    console.log('server-element ngOnChanges');
+    console.log(changes);
+  }
+
+  ngOnInit(): void {
+    console.log('server-element ngOnInit called');
+  }
+
+  ngDoCheck() {
+    console.log('server-element ngDoCheck called');
+  }
+
+  ngAfterContentInit() {
+    console.log('server-element ngAfterContentInit called');
+  }
+
+  ngAfterContentChecked() {
+    console.log('server-element ngAfterContentChecked called');
+  }
+
+  ngAfterViewInit() {
+    console.log('server-element ngAfterViewInit called');
+  }
+
+  ngAfterViewChecked() {
+    console.log('server-element ngAfterViewChecked called');
+  }
+
+  ngOnDestroy(): void {
+    console.log('server-element ngOnDestroy called');
+  }
 }
