213271856c1b10724be7192c99e635d3b0f857a7
zormax <zormax052@gmail.com>
2021-12-10 11:47:20 +0300

    working on ngClass

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -3,13 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-server',
   templateUrl: 'server.component.html',
-  styles: [
-    `
-      .online {
-        color: white;
-      }
-    `,
-  ],
+  styleUrls: ['./server.component.css'],
+  // styles: [
+  //   `
+  //     .online {
+  //       color: white;
+  //     }
+  //   `,
+  // ],
 })
 export class ServerComponent {
   serverId = 10;
