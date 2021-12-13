8defab223ff4064f81a1a2d59b02cc2423e344a5
zormax <zormax052@gmail.com>
2021-12-13 13:00:12 +0300

    end of lesson 135

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,8 +13,9 @@ import { RouterModule, Routes } from '@angular/router';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'users', component: UserComponent },
-  { path: 'servers', component: ServerComponent },
+  { path: 'users', component: UsersComponent },
+  { path: 'users/:id/:name', component: UserComponent },
+  { path: 'servers', component: ServersComponent },
 ];
 
 @NgModule({
