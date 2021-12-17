42478364750f2e9cea6689a0178a5aa436de3b16
zormax <zormax052@gmail.com>
2021-12-17 12:01:33 +0300

    end of section 11

--- src/app/servers/edit-server/can-deactivate-guard.service.ts
+++ src/app/servers/edit-server/can-deactivate-guard.service.ts
@@ -6,7 +6,6 @@ export interface CanComponentDeactivate {
   canDeactivate: () => Observable<boolean> | Promise<boolean> | boolean;
 }
 
-@Injectable({ providedIn: 'root' })
 export class CanDeactivateGuard implements CanDeactivate<CanComponentDeactivate> {
   canDeactivate(
     component: CanComponentDeactivate,
