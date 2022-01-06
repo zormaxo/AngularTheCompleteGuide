59b8dfb14c1c9d55686c9a83cdf52767278f25e5
zormax <zormax052@gmail.com>
2022-01-07 00:07:40 +0300

    end of section 18

---
+++ src/app/auth-interceptor.service.ts
@@ -0,0 +1,9 @@
+import { HttpEvent, HttpEventType, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Observable, tap } from 'rxjs';
+
+export class AuthInterceptorService implements HttpInterceptor {
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const modifiedRequest = req.clone({ headers: req.headers.append('Auth', 'xys') });
+    return next.handle(modifiedRequest);
+  }
+}
---
+++ src/app/logging-interceptor.service.ts
@@ -0,0 +1,19 @@
+import { HttpEvent, HttpEventType, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Observable, tap } from 'rxjs';
+
+export class LoggingInterceptorService implements HttpInterceptor {
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    console.log('Outging Request');
+    console.log(req.url);
+    console.log(req.headers);
+
+    return next.handle(req).pipe(
+      tap((event) => {
+        if (event.type === HttpEventType.Response) {
+          console.log('Incoming Response');
+          console.log(event.body);
+        }
+      })
+    );
+  }
+}
